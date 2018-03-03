8acbed31e9569b6f9c001eea91a84f778118ceb3
Rafael Lage Tavares <rlt.rafael@gmail.com>
2018-03-03 14:01:57 -0300

    add comments and code organization

--- api/controllers/cashMachineController.js
+++ api/controllers/cashMachineController.js
@@ -1,27 +1,40 @@
-exports.list_all_bills = (req, res) => {
-  let amount = req.params.amount;
-  //$ 100,00; $ 50,00; $ 20,00 e $ 10,00
-  let availableNotes = [100, 50, 20, 10];
-  let resultNotes = [];
+/**
+ * Calculates the minimum number of bills needed
+ * to reach the amount
+ * @param {number} amount
+ * @returns {array} returns an array with the bills
+ */
+const calculateBills = (amount) => {
+  let availableBills = [100, 50, 20, 10];
+  let resultBills = [];
   let index = 0;
-  let rest = amount % availableNotes[index];
-  let quo = Math.floor(amount / availableNotes[index]);
-  console.log(quo)
+  let rest = amount % availableBills[index];
+  let quo = Math.floor(amount / availableBills[index]);
+
   while ( rest > 0 || quo > 1){
-    rest = amount % availableNotes[index];
-    quo = Math.floor(amount / availableNotes[index]);
-    console.log("bill ", availableNotes[index]);
-    console.log("rest ", rest);
-    console.log("quo ", quo);
+    rest = amount % availableBills[index];
+    quo = Math.floor(amount / availableBills[index]);
     for (let j = 0; j < quo; j++) {
-      resultNotes.push(availableNotes[index]);
+      resultBills.push(availableBills[index]);
     }
     index++;
     amount = rest;
   }
   if (amount > 0 ) {
-    resultNotes.push(availableNotes[index]);
+    resultBills.push(availableBills[index]);
   }
-  res.json(resultNotes);
-  console.log(resultNotes);
+  return resultBills;
+
+}
+/**
+ * This function receives the requisition and response objects,
+ * calls calculateBills
+ * and respond with a list of bills
+ * @param {object} req
+ * @param {object} res
+ */
+
+exports.list_all_bills = (req, res) => {
+  let amount = req.params.amount;
+  res.json(calculateBills(amount));
 };
