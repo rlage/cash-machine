c48053f7cad69310951ed66f3c67b327019230ff
Rafael Lage Tavares <rlt.rafael@gmail.com>
2018-03-03 12:47:38 -0300

    add working algorithm to return lower number of notes

--- api/controllers/cashMachineController.js
+++ api/controllers/cashMachineController.js
@@ -1,3 +1,26 @@
 exports.list_all_bills = function(req, res) {
-  console.log("list bills for: ", req.params.amount)
+  let amount = req.params.amount;
+  //$ 100,00; $ 50,00; $ 20,00 e $ 10,00
+  let availableNotes = [100, 50, 20, 10];
+  let resultNotes = [];
+  let index = 0;
+  let rest = amount % availableNotes[index];
+  let quo = Math.floor(amount / availableNotes[index]);
+
+  while ( rest > 0){
+    rest = amount % availableNotes[index];
+    quo = Math.floor(amount / availableNotes[index]);
+    console.log("bill ", availableNotes[index]);
+    console.log("rest ", rest);
+    console.log("quo ", quo);
+    for (let j = 0; j < quo; j++) {
+      resultNotes.push(availableNotes[index]);
+    }
+    index++;
+    amount = rest;
+  }
+  if (amount > 0 ) {
+    resultNotes.push(availableNotes[index]);
+  }
+  console.log(resultNotes);
 };
